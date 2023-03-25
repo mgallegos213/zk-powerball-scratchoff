34c574d95a0c894445219cfb2330c89e3b28ae90
Michael Gallegos <michael@mgallegos.dev>
2023-03-25 16:43:57 -0500

    feat: update ticket buying to utilize uint48 for gas optimization

--- contracts/LotteryMap.sol
+++ contracts/LotteryMap.sol
@@ -2,15 +2,18 @@
 pragma solidity ^0.8.0;
 
 contract Powerball {
-    mapping(bytes => address) public tickets;
+    mapping(uint48 => address) public tickets;
     uint public prizePool;
     uint public constant TICKET_PRICE = 0.001 ether;
 
-    constructor() payable {}
-
     function buyTicket(uint8[6] memory numbers) public payable {
-        require(msg.value >= TICKET_PRICE, "Insufficient payment");
-        bytes memory encoded = abi.encodePacked(numbers);
+        require(msg.value == TICKET_PRICE, "Insufficient payment");
+        uint48 encoded = uint48(numbers[0]) << 40 |
+                        uint48(numbers[1]) << 32 |
+                        uint48(numbers[2]) << 24 |
+                        uint48(numbers[3]) << 16 |
+                        uint48(numbers[4]) << 8 |
+                        uint48(numbers[5]);
         tickets[encoded] = msg.sender;
         prizePool += msg.value;
     }
@@ -18,7 +21,12 @@ contract Powerball {
     function drawNumbers() public {
         // Assume the numbers have already been randomly generated
         uint8[6] memory winningNumbers = [1, 2, 3, 4, 5, 6];
-        bytes memory encoded = abi.encodePacked(winningNumbers);
+        uint48 encoded = uint48(winningNumbers[0]) << 40 |
+                        uint48(winningNumbers[1]) << 32 |
+                        uint48(winningNumbers[2]) << 24 |
+                        uint48(winningNumbers[3]) << 16 |
+                        uint48(winningNumbers[4]) << 8 |
+                        uint48(winningNumbers[5]);
         address winner = tickets[encoded];
         if (winner != address(0)) {
             uint prize = prizePool;
@@ -26,7 +34,7 @@ contract Powerball {
             payable(winner).transfer(prize);
         }
     }
-    
+
     receive() external payable {
         // do nothing for now
     }

14ab44f01d92cc217bf6baed90ab5d749698c5d6
Michael Gallegos <michael@mgallegos.dev>
2023-03-28 21:21:29 -0500

    feat: add polybase records for winning numbers

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,9 +2,17 @@
 pragma solidity ^0.8.0;
 
 contract Powerball {
-    mapping(uint48 => address) public tickets;
+    struct Ticket {
+        address owner;
+        uint8[6] numbers;
+        bool isWinner;
+    }
+
+    mapping(uint48 => Ticket) public tickets;
     uint public prizePool;
     uint public constant TICKET_PRICE = 0.001 ether;
+
+    uint8[6] public winningNumbers; // Store the latest drawn numbers
 
     function buyTicket(uint8[6] memory numbers) public payable {
         require(msg.value == TICKET_PRICE, "Insufficient payment");
@@ -14,26 +22,33 @@ contract Powerball {
                         uint48(numbers[3]) << 16 |
                         uint48(numbers[4]) << 8 |
                         uint48(numbers[5]);
-        tickets[encoded] = msg.sender;
+        tickets[encoded] = Ticket(msg.sender, numbers, false);
         prizePool += msg.value;
+        emit TicketBought(encoded, msg.sender);
     }
 
     function drawNumbers() public {
         // Assume the numbers have already been randomly generated
-        uint8[6] memory winningNumbers = [1, 2, 3, 4, 5, 6];
+        winningNumbers = [1, 2, 3, 4, 5, 6];
         uint48 encoded = uint48(winningNumbers[0]) << 40 |
                         uint48(winningNumbers[1]) << 32 |
                         uint48(winningNumbers[2]) << 24 |
                         uint48(winningNumbers[3]) << 16 |
                         uint48(winningNumbers[4]) << 8 |
                         uint48(winningNumbers[5]);
-        address winner = tickets[encoded];
-        if (winner != address(0)) {
+        Ticket storage winningTicket = tickets[encoded];
+        if (winningTicket.owner != address(0)) {
+            winningTicket.isWinner = true;
             uint prize = prizePool;
             prizePool = 0;
-            payable(winner).transfer(prize);
+            payable(winningTicket.owner).transfer(prize);
         }
+        emit DrawingResult(encoded, winningTicket.owner, winningNumbers);
     }
+
+    event TicketBought(uint48 ticket, address buyer);
+
+    event DrawingResult(uint48 encoded, address winner, uint8[6] winningNumbers);
 
     receive() external payable {
         // do nothing for now
--- routes/index.js
+++ routes/index.js
@@ -6,23 +6,70 @@ const fs = require('fs')
 const { ThirdwebSDK } = require('@thirdweb-dev/sdk');
 const { Mumbai } = require('@thirdweb-dev/chains');
 
+const { Polybase } = require('@polybase/client');
+
 const myAbi = [
 	{
+		"anonymous": false,
 		"inputs": [
 			{
+				"indexed": false,
+				"internalType": "uint48",
+				"name": "encoded",
+				"type": "uint48"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "winner",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint8[6]",
+				"name": "winningNumbers",
+				"type": "uint8[6]"
+			}
+		],
+		"name": "DrawingResult",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "TICKET_PRICE",
+		"outputs": [
+			{
 				"internalType": "uint256",
-				"name": "_num",
+				"name": "",
 				"type": "uint256"
 			}
 		],
-		"name": "set",
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint8[6]",
+				"name": "numbers",
+				"type": "uint8[6]"
+			}
+		],
+		"name": "buyTicket",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "drawNumbers",
 		"outputs": [],
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
 		"inputs": [],
-		"name": "get",
+		"name": "prizePool",
 		"outputs": [
 			{
 				"internalType": "uint256",
@@ -34,23 +81,70 @@ const myAbi = [
 		"type": "function"
 	},
 	{
-		"inputs": [],
-		"name": "num",
+		"inputs": [
+			{
+				"internalType": "uint48",
+				"name": "",
+				"type": "uint48"
+			}
+		],
+		"name": "tickets",
 		"outputs": [
 			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "bool",
+				"name": "isWinner",
+				"type": "bool"
 			}
 		],
 		"stateMutability": "view",
 		"type": "function"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "receive"
 	}
 ];
 
 const sdk = new ThirdwebSDK(
   Mumbai,
 );
+
+const db = new Polybase({
+	defaultNamespace: "pk/0x172d0af6327c6435caff388640dcaf6debe07210103a62ba10133ffddf31caf9ba2d0cdd78b536c289c8d272527397aa26ac11965fc007f32d944408e6c98874/Powerball",
+});
+
+const ticketCollection = db.collection("Ticket");
+const drawingCollection = db.collection("Drawing");
+
+async function createTicketRecord(numbers, buyer) {
+	const recordData = await ticketCollection.create([
+	  numbers[0],
+	  numbers[1],
+	  numbers[2],
+	  numbers[3],
+	  numbers[4],
+	  numbers[5],
+	  db.collection("User").record(buyer)
+	]);
+	console.log("Created ticket record:", recordData);
+  }
+  
+  async function createDrawingRecord(winningNumbers) {
+	const recordData = await drawingCollection.create([
+	  winningNumbers[0],
+	  winningNumbers[1],
+	  winningNumbers[2],
+	  winningNumbers[3],
+	  winningNumbers[4],
+	  winningNumbers[5]
+	]);
+	console.log("Created drawing record:", recordData);
+  }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -64,6 +158,22 @@ router.get("/list_movies", async (req, res) => {
 });
 
 router.get("/contract", async (req, res) => {
+	try {
+	  const contract = await sdk.getContractFromAbi("0x8fb4afda5f9eab08e86bf101b00f95fb41463bd7", myAbi);
+	  const result = await contract.call("get");
+	  const data = result.toString(); // Convert BigNumber to string
+	  // ^ Remove .toString() to output the raw data, will be in this format: BigNumber { _hex: '0x05', _isBigNumber: true }
+	  console.log(data);
+	  return res.send(data);
+	} catch (error) {
+	  console.error(error);
+	  return res.status(500).send({ error: "Something went wrong" });
+	}
+  });
+
+  router.get("/draw_powerball", async (req, res) => {
+	// TODO - Store the purchased tickets
+	// Note: These are currently stored in a mapping. Maybe we can listen for the new TicketBought event.
 	try {
 	  const contract = await sdk.getContractFromAbi("0x8fb4afda5f9eab08e86bf101b00f95fb41463bd7", myAbi);
 	  const result = await contract.call("get");
